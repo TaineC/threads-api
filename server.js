c4fc6bbf7b796f5413c5835238b3ac18c085e7dd
Taine Collins <tainecollins@Taines-MacBook-Pro.local>
2019-11-22 16:19:36 +1300

    change database

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ var User = require('./user-model');
 var Review = require('./review-model');
 
 //setup database connection
-var connectionString = 'mongodb://ants:ants@cluster0-shard-00-00-5myjr.mongodb.net:27017,cluster0-shard-00-01-5myjr.mongodb.net:27017,cluster0-shard-00-02-5myjr.mongodb.net:27017/ants?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+var connectionString = 'mongodb+srv://user:blabla123@cluster0-tu99f.mongodb.net/ANTS?retryWrites=true&w=majority';
+
+// 'mongodb://user:blabla123@cluster0-shard-00-00-tu99f.mongodb.net:27017,cluster0-shard-00-01-tu99f.mongodb.net:27017,cluster0-shard-00-02-tu99f.mongodb.net:27017/ANTS?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
 mongoose.connect(connectionString,{ useNewUrlParser: true });
 var  db = mongoose.connection;
 db.once('open', () => console.log('Database connected'));
